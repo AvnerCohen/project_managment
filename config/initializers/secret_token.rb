7259a8959120b29463fd03d6bba2ce736f9249c1
bitnami <bitnami@linux.(none)>
2012-06-02 19:38:34 +0000

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManagment::Application.config.secret_token = '29a937aa0d8ca842ceef3c370bd566622f18183ab1403ab7affc2c8fc5e1338a55c16832925c34c879560823fd494c083072ede870bca261e51fc2342c120549'
