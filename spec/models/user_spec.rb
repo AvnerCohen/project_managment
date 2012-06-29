e40f84bc6b443ba151e723698a7aab99feae7c88
AvnerCohen <israbirding@gmail.com>
2012-06-29 23:35:34 +0300

    Uniquness check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+
+  	it "should validate uniqueness of subdomain" do
+  		@user = User.new(first_name: Faker::Name.first_name,
+  			last_name: Faker::Name.last_name, 
+  			password: "foobar", password_confirmation: "foobar",
+  			email: Faker::Internet.email,
+  			subdomain: "avner")
+  		@user.save.should be_true
+
+  		@user2 = User.new(first_name: Faker::Name.first_name,
+  			last_name: Faker::Name.last_name, 
+  			password: "foobar", password_confirmation: "foobar",
+  			email: Faker::Internet.email,
+  			subdomain: "avner")
+  		@user2.save.should be_false		
+  	end
+
+  end
 end
