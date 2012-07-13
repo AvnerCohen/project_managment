1d82d346d1939988732034e382221402f275f213
AvnerCohen <israbirding@gmail.com>
2012-07-13 22:47:53 +0300

    update gems

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,16 @@
 class Project < ActiveRecord::Base
-	validates_presence_of :name, :status
-	attr_accessible :name, :status
+  validates_presence_of :name, :status
+  attr_accessible :name, :status
 
-	STATUS = {inactive: 0, active: 1, pending: 2, archived: 3}
+  STATUS = {inactive: 0, active: 1, pending: 2, archived: 3}
 
-	def status
-		STATUS.key(read_attribute(:status))
-	end
+  def status
+    STATUS.key(read_attribute(:status))
+  end
 
-	def status=(s)
-		write_attribute(:status, STATUS[s])
-	end
+  def status=(s)
+    write_attribute(:status, STATUS[s])
+  end
+
+  
 end
