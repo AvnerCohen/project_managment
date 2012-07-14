61db1441c37a2684c2e570e1823c3a0a086aada4
AvnerCohen <israbirding@gmail.com>
2012-07-14 22:47:32 +0300

    comment items

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   # attr_accessible :title, :body
+  belongs_to :user
+  belongs_to :item, :polymorphic => true
+  attr_accessible :user_id, :body, :item_id, :item_type
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,9 @@
 class Todo < ActiveRecord::Base
-belongs_to :project
+  belongs_to :project
   #attr_accessible :title, :body
   attr_accessible :title
 
   validates_presence_of :title
+
+  has_many :comments, :as => :item
 end
