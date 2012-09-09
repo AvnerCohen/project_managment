163a2bfb9be01b31b1c0557417fe8afd3fcc9d8e
AvnerCohen <israbirding@gmail.com>
2012-09-09 11:30:20 +0300

    Minorr test changes, not sure why I had to user FactoryGirl.create rather thave 'Factory'

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe DashboardController do
+	render_views
+	include Devise::TestHelpers
 
+
+
+	context "User is signed out" do
+		before(:each) do
+			@request.host = "avner.manage.local"
+			@request.env["HTTP_REFERER"] = @request.host
+		end
+		it "should be redirected" do
+			get :index
+			response.should be_redirect
+		end
+	end
+	context "User is signed in" do
+		before (:each) do
+			@user = FactoryGirl.create(:user, subdomain: "avner")
+
+			sign_in  @user
+			@request.host = "avner.manage.local"
+			@request.env["HTTP_REFERER"] = @request.host
+
+		end
+
+		describe "When user is subdomain owner" do
+
+			it "should allow user to enter" do
+				get :index
+				response.should be_success
+			end
+
+
+		end
+
+		describe "When user is not subdomain owner" do
+		end
+	end
 end
